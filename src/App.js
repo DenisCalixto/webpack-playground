d69abcb4960b2e7ccb65d1521f8fe1403e945275
DenisCalixto <deniscalixto@gmail.com>
2019-10-18 15:50:22 -0700

    Props in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Character from './components/Character'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Webpack App</h1>
+        <Character></Character>
+        <Character></Character>
+        <Character></Character>
       </header>
     </div>
   );
 }
 
+// function Test(num) {
+//   return (
+//     <div>
+//       <h1>This is just a test</h1>
+//     </div>
+//   )
+// }
+
 export default App;
+// export {Test};
---
+++ src/components/Character.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Character.css';
+
+let Character = (props) => {
+    return (
+        <div>
+            <h2>Jack's Bio</h2>
+            <p>Race: Human</p>
+            <p>Status: Healthy</p>
+            <p>Comment: 'Hi'</p>
+        </div>
+    )
+}
+
+export default Character;
